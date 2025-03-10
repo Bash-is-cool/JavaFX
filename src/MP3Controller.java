001f834008c52f21ef616ccbeb9138375bacabfb
fufki <coolddg@duck.com>
2025-03-09 20:03:05 -0500

    Main fix

--- src/MP3Controller.java
+++ src/MP3Controller.java
@@ -1,10 +1,6 @@
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
-        Image icon = new Image("logo.png");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/Main.fxml"));
+        Image icon = new Image("Images/logo.png");
         stage.getIcons().add(icon);
         Parent root = loader.load();
         Scene scene = new Scene(root);
